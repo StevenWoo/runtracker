294585e15ee4bf9474c608042eeb3794e24c3505
Steven Woo <swoo@tackable.com>
2014-05-11 20:45:10 -0700

    skeleton of app

---
+++ app/src/main/java/com/tackable/foobar/android/runtracker/app/RunFragment.java
@@ -0,0 +1,68 @@
+package com.tackable.foobar.android.runtracker.app;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by stevenwoo on 5/11/14.
+ */
+
+public class RunFragment extends Fragment {
+    private static final String TAG = "RunFragment";
+
+    private Location mLastLocation;
+
+    private Button mStartButton, mStopButton;
+    private TextView mStartedTextView, mLatitudeTextView,
+            mLongitudeTextView, mAltitudeTextView, mDurationTextView;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+//        mRunManager = RunManager.get(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_run, container, false);
+
+        mStartedTextView = (TextView)view.findViewById(R.id.run_startedTextView);
+        mLatitudeTextView = (TextView)view.findViewById(R.id.run_latitudeTextView);
+        mLongitudeTextView = (TextView)view.findViewById(R.id.run_longitudeTextView);
+        mAltitudeTextView = (TextView)view.findViewById(R.id.run_altitudeTextView);
+        mDurationTextView = (TextView)view.findViewById(R.id.run_durationTextView);
+
+        mStartButton = (Button)view.findViewById(R.id.run_startButton);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                mRunManager.startLocationUpdates();
+//                mRun = new Run();
+//                updateUI();
+            }
+        });
+
+        mStopButton = (Button)view.findViewById(R.id.run_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                mRunManager.stopLocationUpdates();
+//                updateUI();
+            }
+        });
+
+//        updateUI();
+
+        return view;
+    }
+
+}
